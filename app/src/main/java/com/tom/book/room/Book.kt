4525c1770bb1e7268191561c6891eb3da2bc21c0
Tom <sp9rt77w@gmail.com>
2021-03-16 17:06:08 +0800

    0316_回復到可以跑的版本(無Room)

--- app/src/main/java/com/tom/book/room/Book.kt
+++ app/src/main/java/com/tom/book/room/Book.kt
@@ -1,6 +1,15 @@
 package com.tom.book.room
 
+import androidx.room.Entity
+
+//data class Book(
+//    val contactName:String,
+//    val contactPrice:Int
+//) {} // end data class.
+
+//確認為什麼需要init
+@Entity(tableName = "book_table")
 data class Book(
-    val contactName:String,
-    val contactPrice:Int
-) {} // end data class.
+    var bookName:String = "",
+    var bookPrice:Int = 0
+)
