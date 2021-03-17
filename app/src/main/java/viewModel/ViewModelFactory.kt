90a69c2decd4b5c392e588f188e3cdc310d69f86
Tom <sp9rt77w@gmail.com>
2021-03-17 11:40:43 +0800

    0317_Room做完了，準備要測試是否可以叫出檔案

--- app/src/main/java/viewModel/ViewModelFactory.kt
+++ app/src/main/java/viewModel/ViewModelFactory.kt
@@ -1,15 +1,15 @@
-package viewModel
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-
-
+//package viewModel
 //
-class ViewModelFactory  : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(TitleViewModel::class.java)) {
-            return TitleViewModel() as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
-}
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.ViewModelProvider
+//
+//
+////
+//class ViewModelFactory  : ViewModelProvider.Factory {
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(TitleViewModel::class.java)) {
+//            return TitleViewModel() as T
+//        }
+//        throw IllegalArgumentException("Unknown ViewModel class")
+//    }
+//}
